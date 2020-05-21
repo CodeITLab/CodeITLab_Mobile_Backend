46044183f237e435ebc28410dd9bac3902524f02
Kristijan Fištrek <kristijan.fistrek@t.ht.hr>
2020-05-21 18:04:25 +0200

    #1 - Add: Initial commit

---
+++ src/test/java/com/example/CodeITLab/CodeItLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeITLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeItLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
